0e6b30299738db9304a44cc070f5f098173f68f6
surender77180 <surender791999@gmail.com>
2024-12-12 20:50:16 +0530

    css add

---
+++ index.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.text').forEach(function(event){
+    new Typed(event,{
+    Strings:['Frontend Developer','YouTuber','Web Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+    })
+});
